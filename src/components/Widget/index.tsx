5491b33ae5b6aac98b4ea464712603bfea2ce61f
Armando Magalhães <armando.mag95@gmail.com>
2018-06-19 03:18:30 +0200

    feat: add rebass and grid-styled

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,13 +1,26 @@
 import { h } from "preact"
+import Box from "grid-styled/dist/Box"
+import Flex from "grid-styled/dist/Flex"
+import ButtonOutline from "rebass/dist/ButtonOutline";
+import Provider from "rebass/dist/Provider";
 
 export interface WidgetProps {
   color?: string;
 };
 
 const Widget = (props: WidgetProps) => (
-  <h1 style={{ color: props.color }}>
-    Bug Reporter Widget
-  </h1>
+  <Provider>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Box m={4}>
+        <ButtonOutline>
+          Bug Reporter Widget
+        </ButtonOutline>
+      </Box>
+    </Flex>
+  </Provider>
 );
 
 export default Widget;
