6f895f92f6dfbcd21e8a1bd599d035b7c241d4a0
Guilherme Lima <guilhermelimak@gmail.com>
2018-06-19 02:11:23 -0300

    WIP: More changes to button style
    Add alias to tsconfig

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -1,59 +1,58 @@
-import { h } from "preact";
-import Box from "grid-styled/dist/Box";
-import Flex from "grid-styled/dist/Flex";
-import Button from "rebass/dist/Button";
-import Text from "rebass/dist/Text";
-import Provider from "rebass/dist/Provider";
-import styled from "styled-components";
-import BugIcon from "../BugIcon";
+import { h } from "preact"
+import Box from "grid-styled/dist/Box"
+import Flex from "grid-styled/dist/Flex"
+import Button from "rebass/dist/Button"
+import Provider from "rebass/dist/Provider"
+import styled from "styled-components"
+import BugIcon from "../BugIcon"
 
-export interface WidgetProps {
-  color?: string;
+export enum WidgetState {
+  RECORDING,
+  STOPPED,
+  IDLE
 }
 
-const WidgetButton = Button.extend`
+export interface WidgetProps {
+  state: WidgetState
+}
+
+const size = 40
+
+export const WidgetButton = Button.extend`
   position: fixed;
-  border: 1px solid #ddd;
-  outline: none;
   bottom: 30px;
   right: 30px;
-  width: 40px;
-  padding: 7px 7px;
+  background: white;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  outline: none;
+  height: ${size}px;
+  width: ${size}px;
+  padding: 7px;
   cursor: pointer;
-  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3) !important;
-  height: 40px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.3);
   transition: all 0.4s;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
+  overflow: hidden;
+  white-space: nowrap;
 
   &:hover {
-    box-shadow: 1px 3px 7px rgba(0, 0, 0, 0.2) !important;
+    box-shadow: 1px 3px 7px rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
     width: 130px;
-    background: white;
-
-    & > .wiget__label {
-      opacity: 1;
-    }
   }
-`;
 
-const WidgetLabel = Text.extend`
-  color: #444;
-  opacity: 0 !important;
-`;
-
+  &:active {
+    box-shadow: none;
+  }
+`
 const Widget = (props: WidgetProps) => (
   <Provider>
-    <Flex alignItems="center" justifyContent="center">
-      <Box m={4}>
-        <WidgetButton>
-          <BugIcon />
-          <WidgetLabel className="widget__label">Record bug</WidgetLabel>
-        </WidgetButton>
-      </Box>
-    </Flex>
+    <WidgetButton>
+      <BugIcon />
+    </WidgetButton>
   </Provider>
-);
+)
 
-export default Widget;
+export default Widget
