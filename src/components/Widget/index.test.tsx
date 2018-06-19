db4a1cda4144818fbfc76e4c1af50eca31957437
Guilherme Lima <guilhermelimak@gmail.com>
2018-06-19 02:14:54 -0300

    test: Fix test ^^

--- src/components/Widget/index.test.tsx
+++ src/components/Widget/index.test.tsx
@@ -1,13 +1,14 @@
 /* eslint-env node, jest */
 
-import { h } from "preact";
-import { render } from "preact-render-to-string";
+import { h } from "preact"
+import { render } from "preact-render-to-string"
+import { WidgetState } from "@/components/Widget"
 
-import Widget from "./";
+import Widget from "./"
 
 describe("Widget Snapshot", () => {
   it("should render with content", () => {
-    const tree = render(<Widget />);
-    expect(tree).toMatchSnapshot();
-  });
-});
+    const tree = render(<Widget state={WidgetState.IDLE} />)
+    expect(tree).toMatchSnapshot()
+  })
+})
