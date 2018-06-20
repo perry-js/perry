1c3460870c3ed675a4e9b583adf27e028fa6ce0f
Armando Magalhães <armando.mag95@gmail.com>
2018-06-20 02:15:37 +0200

    feat: Adds plugin support and enhanced Preview
    This commit adds plugins support to Perry.
    
    Now it accepts an options.plugins array property.
    
    Plugins are just plain functions that are executed
    when the user submits the report.
    
    Plugins are functions that receive the report as the only argument.
    
    Removed the options.credentials prop in order to make
    it single purposed.

--- src/interfaces/PerryOptions.ts
+++ src/interfaces/PerryOptions.ts
@@ -1,12 +1,12 @@
 export default interface PerryOptions {
-  credentials: object,
-  clearOnReload: boolean,
   log: boolean,
   warn: boolean,
   error: boolean,
+  clicks: boolean,
   cookies: boolean,
   localStorage: boolean,
   sessionStorage: boolean,
-  clicks: boolean,
-  ignoreScriptErrors: boolean
+  clearOnReload: boolean,
+  ignoreScriptErrors: boolean,
+  plugins: Array<Function>
 };
