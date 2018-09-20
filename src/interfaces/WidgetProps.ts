e00b58e288796d8aab9a427907fbd23a0c3947e2
Vitor Margis <vitor.margis@gmail.com>
2018-09-21 00:16:10 +0200

    Implement recording start

--- src/interfaces/WidgetProps.ts
+++ src/interfaces/WidgetProps.ts
@@ -2,6 +2,7 @@ import WidgetStatus from "@/interfaces/WidgetStatus";
 import PerryReportInfo from "@/interfaces/PerryReportInfo";
 
 export default interface WidgetProps {
-  status?: WidgetStatus;  
+  status?: WidgetStatus;
   onSubmit?: (info: PerryReportInfo) => void,
+  onStartRecording: () => void
 };
--- src/packages/render-widget/index.tsx
+++ src/packages/render-widget/index.tsx
@@ -1,13 +1,16 @@
-import { h } from "preact"; 
+import { h } from "preact";
 import habitat from "preact-habitat";
 import Widget from "../../components/Widget";
 import WidgetProps from "@/interfaces/WidgetProps";
 
 const renderWidget = (props: WidgetProps) => {
   const Wrapper = () => (
-    <Widget onSubmit={props.onSubmit} />
+    <Widget
+      onStartRecording={props.onStartRecording}
+      onSubmit={props.onSubmit}
+    />
   );
-  
+
   const { render } = habitat(Wrapper);
 
   render({
