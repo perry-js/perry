f979e9179330810a852a01a219612b71558fa569
Armando Magalhães <armando.mag95@gmail.com>
2018-09-23 18:51:02 +0200

    fix: refactor and simplify render-widget package

--- src/packages/render-widget/index.tsx
+++ src/packages/render-widget/index.tsx
@@ -4,9 +4,7 @@ import Widget from "@/components/Widget";
 import WidgetProps from "@/interfaces/WidgetProps";
 
 const renderWidget = (props: WidgetProps) => {
-  const { render } = habitat(() => <Widget {...props} />);
-
-  render({
+  habitat(() => <Widget {...props} />).render({
     selector: 'body',
     clean: false,
   });
