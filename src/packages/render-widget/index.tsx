22c0b9873e241d1fd16f6b1e4f2450ab175a52f7
Armando Magalhaes <amagalhaes@travix.com>
2019-03-13 17:19:23 +0100

    docs: better page

--- src/packages/render-widget/index.tsx
+++ src/packages/render-widget/index.tsx
@@ -2,10 +2,24 @@ import Widget from "@/components/Widget";
 import IWidgetProps from "@/interfaces/IWidgetProps";
 import { h, render } from "preact";
 
-const renderWidget = (props: IWidgetProps) => {
+const PERRY_WIDGET_CONTAINER_ID = "perry-widget";
+
+const queryWidgetContainer = () =>
+  document.getElementById(PERRY_WIDGET_CONTAINER_ID);
+
+const createWidgetContainer = () =>  {
   const container = document.createElement("div");
+  container.setAttribute("id", PERRY_WIDGET_CONTAINER_ID);
   document.body.appendChild(container);
-  render(<Widget {...props} />, container);
+  return container;
+};
+
+const renderWidget = (props: IWidgetProps) => {
+  const shouldRender = queryWidgetContainer() === null;
+
+  if (shouldRender) {
+    render(<Widget {...props} />, createWidgetContainer());
+  }
 };
 
 export default renderWidget;
