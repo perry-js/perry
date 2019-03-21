7c551a55257fcb898f7abacfc4b116782027eaf6
Armando Magalhaes <amagalhaes@travix.com>
2019-03-21 04:27:46 +0100

    refactor: remvoe yup

--- packages/perry/src/lib/is-valid-options/index.ts
+++ packages/perry/src/lib/is-valid-options/index.ts
@@ -1,5 +1,59 @@
-import { IPerryOptions} from "@perry/perry-interfaces";
-import PerryOptionsSchema from "../options-schema";
+import { IPerryOptions } from "@perry/perry-interfaces";
 
-export default (options: IPerryOptions): boolean =>
-  PerryOptionsSchema.isValidSync(options, { strict: true });
+const expectedTypes = {
+  clearOnReload: "boolean",
+  clearOnStart: "boolean",
+  clicks: "boolean",
+  cookies: "boolean",
+  enableScreenRecording: "boolean",
+  error: "boolean",
+  ignoreScriptErrors: "boolean",
+  localStorage: "boolean",
+  log: "boolean",
+  plugins: "array",
+  sessionStorage: "boolean",
+  warn: "boolean",
+};
+
+function isArray(value) {
+  return value && typeof value === "object" && value.constructor === Array;
+}
+
+function throwError(property, currentValue, expectedType) {
+  throw new Error(
+    `[Perry Options]: "${property}" was supposed to be of type "${expectedType}", but received "${currentValue}"`,
+  );
+}
+
+function shouldThrow(currentValue: any, expectedType: "array" | "boolean") {
+  const currentType = typeof currentValue;
+
+  const isNotAnArray = expectedType === "array" && !isArray(currentValue);
+  const isNotExpectedType = currentType !== expectedType;
+
+  return isNotAnArray || isNotExpectedType;
+}
+
+const checkOptions = (options: IPerryOptions) => {
+  for (const property in options) {
+    if (!options.hasOwnProperty(property)) {
+      const currentValue = options[property];
+      const expectedType = expectedTypes[property];
+
+      if (shouldThrow(currentValue, expectedType)) {
+        throwError(property, currentValue, expectedType);
+      }
+    }
+  }
+};
+
+export default (options: IPerryOptions): boolean => {
+  try {
+    checkOptions(options);
+    return true;
+  } catch (e) {
+    /* tslint:disable-next-line */
+    console.error(e);
+    return false;
+  }
+}
